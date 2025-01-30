2d73d6604e90611d6dd6b3e7acc37d082b37374f
sharathreddyt <sharathsdreamsden@gmail.com>
2025-01-30 15:54:58 -0600

    Initial changes to tic tac toe game

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const statusDisplay = document.querySelector('.status');
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+function handleCellPlayed{}
+function handlePlayerChange{}
